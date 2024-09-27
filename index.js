47bb9a2225412383b39ce3180da7ea76b30672ef
Gulzarokz <gokz22301@gamil.com>
2024-09-27 20:54:57 +0500

    add list items and remove button

--- index.js
+++ index.js
@@ -4,7 +4,23 @@ function addTask() {
     
     // if some one want to enter empty space 
     if (!taskInput.value.trim()) {
-     return alert("Todo can't be empty!")
+     return alert("Please enter some text! Todo can't be empty")
            
     }
+    // New list item Here
+    var li = document.createElement('li');
+    li.textContent = taskInput.value;
+
+    // Create a remove button Here
+    var removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.className = 'remove-btn';
+    removeButton.onclick = function () {
+        taskList.removeChild(li);
+    };
+
+    // Add click event to mark as completed
+    li.onclick = function () {
+        li.classList.toggle('completed');
+    };
 }
